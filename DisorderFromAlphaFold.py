64e74a8ee48b411d5b3ac9bbd95af6e5671fa5f9
Jack <jackent601@gmail.com>
2023-02-09 15:46:48 +0000

    initial commit, disorder fraction calculation

---
+++ DisorderFromAlphaFold.py
@@ -0,0 +1,86 @@
+import numpy as np
+from Bio.PDB import PDBParser
+# ================================================================================================================================
+#   DISORDER (pLDDT) Fractions
+# ================================================================================================================================
+
+def getpLDDTsFromAlphaFoldPDBModel(pdb_model):
+    """
+    AlphaFold stores plDDT as B-factor, pLDDT calculated per residue to only need to query first atom, returns array of pLDDT values (per residue)
+    """
+    return np.array([next(r.get_atoms()).get_bfactor() for r in pdb_model.get_residues()])
+
+def getpLDDTsSubSequenceFromAlphaFoldPDBModel(pdb_model, startRes=None, endRes=None):
+    """
+    Returns pLDDTs of a sub-sequence within AF model, note: python is zero indexed, residue numbers are not!
+    """
+    if endRes is None and startRes is None:
+        # read whole sequence
+        return getpLDDTsFromAlphaFoldPDBModel(pdb_model)
+    else:
+        # filter for subsequence
+        assert startRes is not None and endRes is not None, "Must provide BOTH of start res and end res if specified"
+        return getpLDDTsFromAlphaFoldPDBModel(pdb_model)[startRes-1:endRes]
+    
+def getConsecutiveDisorderLengthsFrompLDDTs(pLDDTs, pLDDTDisorderThreshold):
+    """
+    finds the length of all stretches of residues consecutively below a pLDDT threshold (i.e. 'disordered' stretches) 
+    pLDDTs should be an array of pLDDTs from AF PDB file
+    """
+    # Finds indices in pLDDT list that are below threshold
+    LowpLDDTIndices = np.argwhere(pLDDTs <= pLDDTDisorderThreshold)[:, 0]
+
+    # Need to duplicate final index value for calculating the lengths below in while loop 
+    LowpLDDTIndices = np.append(LowpLDDTIndices, LowpLDDTIndices[-1])
+
+    # Find where difference between adjacent indices is greater than 1 
+    # (This indicates the begining of a new stretch of consecutively low pLDDTs regions)
+    # (Adds one to correct for shifting array)
+    consecutiveLowpLDDTIndices = np.where(LowpLDDTIndices[1:] - LowpLDDTIndices[:-1] > 1)[0] + 1
+
+    # Prepends with zero to account for first length
+    consecutiveLowpLDDTIndices = np.insert(consecutiveLowpLDDTIndices, 0, 0)
+
+    # Calculate length of each consecutively low pLDDT sequence
+    consecutiveLens = np.zeros(len(consecutiveLowpLDDTIndices)).astype(np.int64)
+    for i in range(len(consecutiveLowpLDDTIndices)):
+        _length = 1
+        _idx = consecutiveLowpLDDTIndices[i]
+        while LowpLDDTIndices[_idx+1] == LowpLDDTIndices[_idx] + 1:
+            _length += 1
+            _idx += 1
+        consecutiveLens[i] = _length
+    return consecutiveLens
+
+def getDisorderedFractionFrompLDDTs(pLDDTs, pLDDTDisorderThreshold, numberConsectuivelyDisorderThreshold):
+    """
+    Uses getConsecutiveDisorderLengthsFrompLDDTs to get length of all stretches of residues consecutively below a pLDDT threshold (i.e. 'disordered' stretches) 
+    Then filters lengths for those above or equal to length threshold
+    Returns the disordered 'fraction', along with raw lengths of disordered residues within stretches above threshold
+    """
+    disorderedLengths = getConsecutiveDisorderLengthsFrompLDDTs(pLDDTs, pLDDTDisorderThreshold)
+    
+    disorderedLengthsFiltered = disorderedLengths[disorderedLengths >= numberConsectuivelyDisorderThreshold]
+    
+    return sum(disorderedLengthsFiltered)/len(pLDDTs), disorderedLengthsFiltered
+
+# ================================================================================================================================
+#   Functions From PDB Paths
+# ================================================================================================================================
+
+def getDisorderedFractionFromPDB(PDBpath, pLDDTDisorderThreshold, numberConsectuivelyDisorderThreshold, startRes=None, endRes=None):
+    """
+    getDisorderedFractionFrompLDDTs but reads directly from a PDB path, including option to sub sequence
+    """
+    # Read PDB
+    parser = PDBParser()
+    structure = parser.get_structure('auto_read', PDBpath)
+
+    # Get Model, AlphaFold PDBs only have 1
+    pdb_model = structure[0]
+
+    # Get pLDDTs
+    pLDDTs = getpLDDTsSubSequenceFromAlphaFoldPDBModel(pdb_model, startRes=startRes, endRes=endRes)
+
+    # Get 'disordered' fractions
+    return getDisorderedFractionFrompLDDTs(pLDDTs, pLDDTDisorderThreshold, numberConsectuivelyDisorderThreshold)
